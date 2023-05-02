b8b5f901ebbe7f5815bd1679193e32b44ca615ee
nate <70911747+trapnvte@users.noreply.github.com>
2023-05-02 19:06:10 -0400

    modified BST implementation

--- Assignment2/BinarySearchTree.cpp
+++ Assignment2/BinarySearchTree.cpp
@@ -16,7 +16,7 @@ public:
     BinarySearchTree() {
         root = nullptr;
     }
-  // Time complexity: 
+  // Time complexity: O(n)
   int min() {
     if (root == nullptr) {
       return 0;
@@ -28,7 +28,7 @@ public:
     return cur->val;
   }
 
-  // Time complexity: 
+  // Time complexity: O(n)
   int max() {
     if (root == nullptr) {
       return 0;
@@ -40,7 +40,7 @@ public:
     return cur->val;
   }
 
-  // Time complexity: 
+  // Time complexity: O(n)
   bool contains(int val) {
     if (root == nullptr) {
       return false;
@@ -56,8 +56,50 @@ public:
     return cur->val == val;
   }
 
-  // Time complexity: 
-  void insert(int val);
-  // Time complexity: 
+  // Time complexity: O(n)
+  void insert(int val) {
+
+    // if root is null, create new node and set as root
+    if (root == nullptr) {
+      root = new node_t;
+      root->val = val;
+      root->left = nullptr;
+      root->right = nullptr;
+      return;
+    }
+
+    node_t *cur = root;
+    node_t* ins = new node_t;
+
+    // find the correct place to insert the node
+    while (cur != nullptr) {
+
+      // if value less than current node's value
+      if (val < cur->val) {
+        if (cur->left == nullptr) {
+          ins->val = val;
+          ins->left = nullptr;
+          ins->right = nullptr;
+          cur->left = ins;
+          return;
+        }
+        cur = cur->left;
+
+      } else {
+
+        // if value greater than current node's value
+        if (cur->right == nullptr) {
+          ins->val = val;
+          ins->left = nullptr;
+          ins->right = nullptr;
+          cur->right = ins;
+          return;
+        }
+
+        cur = cur->right;
+      }
+    }
+  }
+  // Time complexity: O(n)
   int deleteNode(int val);
 };
